78379430f04448e17820792801b9122d05b6521c
YY <37497289+avt1745zet@users.noreply.github.com>
2022-04-03 23:22:14 +0800

    Remove Index.tsx, because need to rename it to index.tsx

--- src/Index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot, Root } from "react-dom/client";
-import App from "./App";
-
-const container: HTMLElement | null = document.getElementById("root");
-if(container){
-	const root: Root = createRoot(container);
-	root.render(<App/>);
-}
