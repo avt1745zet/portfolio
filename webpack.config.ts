39f402e87ea7033efb1f86556d135d06d871bc58
YY <37497289+avt1745zet@users.noreply.github.com>
2022-04-03 23:27:02 +0800

    Add packages: bootstrap, css-loader, style-loader

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,10 +3,11 @@ import * as webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const config: webpack.Configuration = {
-	entry: "./src/Index.tsx",
+	entry: "./src/index.tsx",
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "bundle.js",
+		assetModuleFilename: "assets/[name]_[hash][ext][query]"
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
@@ -21,11 +22,20 @@ const config: webpack.Configuration = {
 				use: "ts-loader",
 				exclude: /node_modules/,
 			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.png/,
+				type: "asset/resource"
+			}
 		],
 	},
 	resolve: {
 		extensions: [".tsx", ".ts", ".js"],
 	},
+	devtool: "eval-source-map",
 	mode: "development",
 };
 
