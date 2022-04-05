cf319c224a58d7b10ccd7a5cb0f4862b4ebca77f
YY <37497289+avt1745zet@users.noreply.github.com>
2022-04-06 00:29:39 +0800

    Update projects section

--- src/pages/main/Projects.tsx
+++ src/pages/main/Projects.tsx
@@ -1,22 +1,60 @@
 import React from "react";
-import { Col, Container, ContainerProps, Row } from "react-bootstrap";
+import { Card, Col, Container, ContainerProps, Row } from "react-bootstrap";
 
 const Projects = (props: ContainerProps) => {
 	const {...other} = props;
+	const projectInfoData: Array<IProjectInfo> = new Array<IProjectInfo>(
+		{
+			name: "Project name 1",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, esse placeat officia rem ipsum officiis at culpa iusto vero fuga, Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, esse placeat officia rem ipsum officiis at culpa iusto vero fuga.",
+			imageSrc: "http://placehold.jp/640x360.png"
+		},
+		{
+			name: "Project name 2",
+			description: "Amet consectetur adipisicing elit. Modi, esse placeat officia rem ipsum officiis at culpa iusto vero fuga.",
+			imageSrc: "http://placehold.jp/640x360.png"
+		},
+		{
+			name: "Project name 3",
+			description: "Lorem ipsum dolor sit amet consectetur, Lorem ipsum dolor sit amet consectetur, Lorem ipsum dolor sit amet consectetur.",
+			imageSrc: "http://placehold.jp/640x360.png"
+		},
+		{
+			name: "Project name 4",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, esse placeat officia rem ipsum officiis at culpa iusto vero fuga.",
+			imageSrc: "http://placehold.jp/640x360.png"
+		}
+	);
 	return (
-		<Container {...other}>
-			<Row {...other} className="text-light">
-				<Col sm={12}>
-					<h2 className="text-center" >
-                    Projects
-					</h2>
-					<p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non unde, quibusdam dolorem error magni numquam aliquid nam odit veniam esse? Velit ratione perspiciatis qui excepturi earum tempora, eius mollitia impedit!
-					</p>
-				</Col>
+		<Container {...other} className="text-light">
+			<Container as="header">
+				<h2 className="text-center" >
+					Projects
+				</h2>
+			</Container>
+			<Row className="g-3">
+				{
+					projectInfoData.map((data, index)=> (
+						<Col key={index} sm={12} md={6} lg={4}>
+							<Card bg="dark">
+								<Card.Img src={data.imageSrc}/>
+								<Card.Body>
+									<Card.Title>{data.name}</Card.Title>
+									<Card.Text>{data.description}</Card.Text>
+								</Card.Body>
+							</Card>
+						</Col>
+					))
+				}
 			</Row>
 		</Container>
 	);
 };
 
 export default Projects;
+
+interface IProjectInfo {
+	name: string;
+	description: string;
+	imageSrc: string;
+}
