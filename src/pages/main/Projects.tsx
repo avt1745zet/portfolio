86d74d179c77ce942023a1d13d1e4542d0733602
YY <37497289+avt1745zet@users.noreply.github.com>
2022-04-11 00:36:12 +0800

    Add project type

--- src/pages/main/Projects.tsx
+++ src/pages/main/Projects.tsx
@@ -6,25 +6,47 @@ const Projects: FC<ComponentProps<"section">> = (props: ComponentProps<"section"
 	const projectInfoData: Array<IProjectInfo> = new Array<IProjectInfo>(
 		{
 			name: "Project name 1",
+			type: ProjectType.WEB,
 			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, esse placeat officia rem ipsum officiis at culpa iusto vero fuga, Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, esse placeat officia rem ipsum officiis at culpa iusto vero fuga.",
 			imageSrc: "http://placehold.jp/640x360.png"
 		},
 		{
 			name: "Project name 2",
+			type: ProjectType.GAME,
 			description: "Amet consectetur adipisicing elit. Modi, esse placeat officia rem ipsum officiis at culpa iusto vero fuga.",
 			imageSrc: "http://placehold.jp/640x360.png"
 		},
 		{
 			name: "Project name 3",
+			type: ProjectType.TOOL,
 			description: "Lorem ipsum dolor sit amet consectetur, Lorem ipsum dolor sit amet consectetur, Lorem ipsum dolor sit amet consectetur.",
 			imageSrc: "http://placehold.jp/640x360.png"
 		},
 		{
 			name: "Project name 4",
+			type: ProjectType.GAME,
 			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, esse placeat officia rem ipsum officiis at culpa iusto vero fuga.",
 			imageSrc: "http://placehold.jp/640x360.png"
 		}
 	);
+	const getTypeText = (type: ProjectType) => {
+		let result: string;
+		switch (type) {
+		case ProjectType.GAME:
+			result = "Game";
+			break;
+		case ProjectType.WEB:
+			result = "Web";
+			break;
+		case ProjectType.TOOL:
+			result = "Tool";
+			break;
+		default:
+			result = "uncategorized";
+			break;
+		}
+		return result;
+	};
 	return (
 		<section {...other} className="text-light my-5">
 			<header>
@@ -33,13 +55,17 @@ const Projects: FC<ComponentProps<"section">> = (props: ComponentProps<"section"
 				</h2>
 			</header>
 			<Row className="g-3">
+				<Col sm={12}>
+					<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa nemo rerum quae iusto eaque vero non eos placeat.</p>
+				</Col>
 				{
 					projectInfoData.map((data, index)=> (
 						<Col key={index} sm={12} md={6} lg={4}>
-							<Card bg="dark">
+							<Card bg="dark" text="light">
 								<Card.Img src={data.imageSrc}/>
 								<Card.Body>
 									<Card.Title>{data.name}</Card.Title>
+									<Card.Subtitle className="mb-2 text-muted">{`Type: ${getTypeText(data.type)}`}</Card.Subtitle>
 									<Card.Text>{data.description}</Card.Text>
 								</Card.Body>
 							</Card>
@@ -55,6 +81,13 @@ export default Projects;
 
 interface IProjectInfo {
 	name: string;
+	type: ProjectType;
 	description: string;
 	imageSrc: string;
+}
+
+enum ProjectType {
+	GAME,
+	WEB,
+	TOOL
 }
