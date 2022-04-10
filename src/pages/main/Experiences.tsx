72f628d2feed6d00606a18a1bc7d7804615c4998
YY <37497289+avt1745zet@users.noreply.github.com>
2022-04-11 00:11:55 +0800

    Update experiences content

--- src/pages/main/Experiences.tsx
+++ src/pages/main/Experiences.tsx
@@ -31,24 +31,15 @@ const Experiences: FC<ComponentProps<"section">> = (props: ComponentProps<"secti
 				<Col xs={12} lg={6}>
 					<Timeline items={timelineItemData}/>
 				</Col>
-				<Col xs={12} lg={6}>
+				<Col xs={12} lg={6} className="align-self-center">
 					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Et molestias vero incidunt maiores natus adipisci fugiat consectetur, quam exercitationem illum iste iusto? Repudiandae, vitae est, aliquam placeat libero quisquam aspernatur delectus dolore corrupti blanditiis dicta debitis amet mollitia voluptates perspiciatis! Est enim aliquam esse cum magnam asperiores velit nesciunt ut.
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis dolorum odio voluptate, sapiente consequuntur vitae!
 					</p>
-				</Col>
-				<Col xs={12}>
 					<p>
-						Adipisicing elit. Incidunt cupiditate quos quasi suscipit odit quia omnis labore consequatur, rem dolorum praesentium mollitia velit minima dolores nulla fuga vel maiores deserunt.
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, iste?
 					</p>
-				</Col>
-				<Col xs={12}>
 					<p>
-						Quia omnis labore consequatur, rem dolorum praesentium mollitia velit minima dolores nulla fuga vel maiores deserunt.
-					</p>
-				</Col>
-				<Col xs={12}>
-					<p>
-						Ipsum dolor sit amet consectetur adipisicing elit. Inventore sunt doloribus, voluptas officiis cumque ipsum animi ipsa facilis, totam non similique rem necessitatibus nisi cum, tempore delectus praesentium aspernatur quae adipisci eum deleniti deserunt sit voluptatibus. Ab unde odit quaerat ullam debitis, numquam voluptate consequuntur eum aliquam sint minus quisquam natus? Molestias tempore minus explicabo eius omnis, aliquid possimus optio.
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia minus, dolor nobis dolorum aspernatur atque! Recusandae vero nostrum eveniet itaque quasi at praesentium doloribus sapiente?
 					</p>
 				</Col>
 			</Row>
