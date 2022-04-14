9553a52ca4996614472ec4e3abc6671e6f03debb
YY <37497289+avt1745zet@users.noreply.github.com>
2022-04-14 17:30:49 +0800

    Use StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import { createRoot, Root } from "react-dom/client";
 import App from "./App";
 
 const container: HTMLElement | null = document.getElementById("root");
 if(container){
 	const root: Root = createRoot(container);
-	root.render(<App/>);
+	root.render(
+		<StrictMode>
+			<App/>
+		</StrictMode>
+	);
 }
